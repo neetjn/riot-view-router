f802c19742dce028749c4b8b9fade4d65cfc70a0
John Nolette <john@neetgroup.net>
2017-10-31 01:09:10 -0400

    Completed basic karma tests

--- test/e2e/spec.route.js
+++ test/e2e/spec.route.js
@@ -4,14 +4,23 @@ describe('riot-view-router', function() {
     mocks.tags.forEach(function(tag) {
       riot.tag(tag.name, tag.template)
     }) // # create our mock tags
-    riot.mixin(new Router(mocks.options, mocks.states))
+    riot.mixin('router', new Router(mocks.options, mocks.states))
     var html = document.createElement('app')
     document.body.appendChild(html)
     riot.mount('app')
   })
 
-  it('should mount the tag', function() {
+  it('should not start until called', function() {
+    expect(document.querySelector('app r-view')).toBeDefined()
+  })
 
+  it('should instantiate property "$router"', function() {
+    riot.mixin('router').$router
+  })
+
+  it('should navigate to default state on start', function() {
+    riot.mixin('router').$router.start()
+    expect(document.querySelector('home')).toBeDefined()
   })
 
 })
