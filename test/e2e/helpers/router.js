d584a1122b7358eaecc00999760aced5417d8e1b
John Nolette <john@neetgroup.net>
2017-11-05 20:24:55 -0500

    Cleaned up karma tests, added helpers.

---
+++ test/e2e/helpers/router.js
@@ -0,0 +1,13 @@
+failAsyncTest = function(error) {
+  expect(error).toBeUndefined()
+}
+
+/** Assert current route using router's hash pattern */
+isLocation = function(location) {
+  expect(window.location.hash).toBe(router.$constants.defaults.hash + location)
+}
+
+/** Assert element has been rendered from the router's context */
+isRendered = function(tagName) {
+  expect(document.querySelector(router.$constants.defaults.marker + ' ' + tagName)).not.toBeNull()
+}
---
+++ test/e2e/spec.lifecycle.js
@@ -0,0 +1,8 @@
+describe('riot-view-router lifecycle events', function() {
+
+  beforeEach(setUp)
+  afterEach(tearDown)
+
+  it('should have')
+
+})
