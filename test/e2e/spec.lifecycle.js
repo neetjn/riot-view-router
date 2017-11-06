10d42d512d9aaf5eb639bc85f7163e5a04296d62
John Nolette <john@neetgroup.net>
2017-11-05 20:50:09 -0500

    Finished test for lifecycle event handler

--- test/e2e/spec.lifecycle.js
+++ test/e2e/spec.lifecycle.js
@@ -6,10 +6,8 @@ describe('riot-view-router lifecycle events', function() {
   it('handlers should be available once registered', function(done) {
     var check = { count: 0 }
     function handler() { check.count += 1 }
-    console.log('ayyy')
     router.on('navigation', handler).then(() => {
-      console.log('made it?')
-      expect(router.$events.transition).toBe(handler)
+      expect(router.$events.navigation).toBe(handler)
       router.navigate('/about').then(() => {
         expect(check.count).toBe(1)
         done()
