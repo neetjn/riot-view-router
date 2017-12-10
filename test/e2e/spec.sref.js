4314fa02317b9f2307029c9b002b519468362700
John Nolette <john@neetgroup.net>
2017-12-10 14:42:10 -0500

    Added new test for sref by state name

--- test/e2e/spec.sref.js
+++ test/e2e/spec.sref.js
@@ -18,4 +18,19 @@ describe('riot-view-router state reference', function() {
       })
     })
 
+    it('should properly redirect to state route', function(done) {
+      var called = false
+      router.on('navigation', () => {
+        called = true
+      }).then(() => {
+        document.querySelector('button[r-sref]').click()
+        var locationCheck = setInterval(() => {
+          expect(called).toBeTruthy()
+          isLocation('/profile/view/someguy')
+          window.clearInterval(locationCheck)
+          done()
+        }, router.constants.intervals.navigate)
+      })
+    })
+
   })
