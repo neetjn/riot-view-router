70af646947810646a42aab9a966bcaba0a3b0437
John Nolette <john@neetgroup.net>
2017-12-10 15:15:51 -0500

    Updated test, removed callback

--- test/e2e/spec.sref.js
+++ test/e2e/spec.sref.js
@@ -3,7 +3,7 @@ describe('riot-view-router state reference', function() {
     beforeEach(setUp)
     afterEach(tearDown)
 
-    it('should bind target elements', function(done) {
+    it('should bind target elements and redirect to given route', function(done) {
       var called = false
       router.on('navigation', () => {
         called = true
@@ -18,19 +18,13 @@ describe('riot-view-router state reference', function() {
       })
     })
 
-    it('should properly redirect to state route', function(done) {
-      var called = false
-      router.on('navigation', () => {
-        called = true
-      }).then(() => {
-        document.querySelector('button[r-sref]').click()
-        var locationCheck = setInterval(() => {
-          expect(called).toBeTruthy()
-          isLocation('/profile/view/someguy')
-          window.clearInterval(locationCheck)
-          done()
-        }, router.constants.intervals.navigate)
-      })
+    it('should redirect to given state by name', function(done) {
+      document.querySelector('button[r-sref]').click()
+      var locationCheck = setInterval(() => {
+        isLocation('/profile/view/someguy')
+        window.clearInterval(locationCheck)
+        done()
+      }, router.constants.intervals.navigate)
     })
 
   })
