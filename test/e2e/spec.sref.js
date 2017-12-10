22e71ad34a087050f08de8e8147fe208c1aee2a9
John Nolette <john@neetgroup.net>
2017-12-10 15:26:19 -0500

    Updated home mock template to include about sref anchor

--- test/e2e/spec.sref.js
+++ test/e2e/spec.sref.js
@@ -1,30 +1,30 @@
 describe('riot-view-router state reference', function() {
 
-    beforeEach(setUp)
-    afterEach(tearDown)
+  beforeEach(setUp)
+  afterEach(tearDown)
 
-    it('should bind target elements and redirect to given route', function(done) {
-      var called = false
-      router.on('navigation', () => {
-        called = true
-      }).then(() => {
-        document.querySelector('button[r-sref]').click()
-        var locationCheck = setInterval(() => {
-          expect(called).toBeTruthy()
-          isLocation('/profile/view/someguy')
-          window.clearInterval(locationCheck)
-          done()
-        }, router.constants.intervals.navigate)
-      })
-    })
-
-    it('should redirect to given state by name', function(done) {
+  it('should bind target elements and redirect to given route', function(done) {
+    var called = false
+    router.on('navigation', () => {
+      called = true
+    }).then(() => {
       document.querySelector('button[r-sref]').click()
       var locationCheck = setInterval(() => {
+        expect(called).toBeTruthy()
         isLocation('/profile/view/someguy')
         window.clearInterval(locationCheck)
         done()
       }, router.constants.intervals.navigate)
     })
-
   })
+
+  it('should redirect to given state by name', function(done) {
+    document.querySelector('button[r-sref]').click()
+    var locationCheck = setInterval(() => {
+      isLocation('/profile/view/someguy')
+      window.clearInterval(locationCheck)
+      done()
+    }, router.constants.intervals.navigate)
+  })
+
+})
