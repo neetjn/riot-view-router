01a6672c123f43a6601405b9beb6161fbd3b0b39
John Nolette <john@neetgroup.net>
2018-02-02 14:28:50 -0500

    Cleaned up npm scripts

--- src/core.js
+++ src/core.js
@@ -190,7 +190,6 @@ export default class {
       if (self.$state && self.$state.onLeave)
         self.$state.onLeave(state) // # call onLeave, pass old state
 
-      console.log('\n\nmade it\n\n')
       self.$tools.transition(state, opts)
 
       if (state.onEnter)
